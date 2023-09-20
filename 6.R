63488f2c66080ecbff319c169a3618040abefe57
Urifoz <144878994+Urifoz@users.noreply.github.com>
2023-09-20 14:57:44 +0800

    upload

---
+++ 6.R
@@ -0,0 +1,3 @@
+dt1 <- readr::read_csv("https://raw.githubusercontent.com/tpemartin/112-1-programming-for-data-science/main/data/cities.
+                       dt2 <- jsonlite::fromJSON("https://raw.githubusercontent.com/tpemartin/112-1-programming-for-data-science/main/data/foodp
+                       
